4caf10397ef362dc25971eada6a41988ec45ae4b
ctmatias <ctmatias@ncsu.edu>
2024-07-05 21:25:01 -0400

    Initial commit

---
+++ src/ui/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <p>exercise app project</p>
+    </div>
+  );
+}
+
+export default App;
