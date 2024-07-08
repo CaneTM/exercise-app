b4f0d22f23aeadd2596d3b48c4e64c77e5af842f
PotatoKingX2 <63024325+PotatoKingX2@users.noreply.github.com>
2024-07-08 15:54:44 -0400

    fortnite balls

--- src/test-data/users.js
+++ src/test-data/users.js
@@ -4,7 +4,47 @@ let users = {
         'name': 'Tim',
         'password': 'asdf',
         'age': 20,
-        'maxTime': 0,
-        'distance': 0
+        'maxTime': 0,   //time in minutes
+        'distance': 0   //distance in km
     }
+    ,
+
+    'user1': {
+        'name': 'Friedrich',
+        'password': 'fortnite',
+        'age': 36,
+        'maxTime': 10,
+        'distance': 5
+    }
+    ,
+
+    'user2': {
+        'name': 'Geoffrey',
+        'password': 'NotSecretlyG@y',
+        'age': 16,
+        'maxTime': 4,
+        'distance': 1
+    }
+    ,
+
+    'user3': {
+        'name': 'Kalsomere',
+        'password': 'Money&noBiches',
+        'age': 55,
+        'maxTime': 60,
+        'distance': 12
+    }
+    ,
+
+    'user4': {
+        'name': 'Patrick',
+        'password': 'password',
+        'age': 6,
+        'maxTime': 1,
+        'distance': 0.03
+    }
+
 }
+
+export default users
+
